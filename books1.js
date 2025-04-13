f8fe030917031c988d24d835c9a07c6d166077a2
miwanoff <marina.iva.noffa@gmail.com>
2025-04-13 15:38:07 +0300

    Add file creation

--- books1.js
+++ books1.js
@@ -1,6 +1,6 @@
 function loadBooks() {
     let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "books.json", true);
+    xhttp.open("GET", "books1.json", true);
     xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8");
     xhttp.send();
     xhttp.onreadystatechange = function() {
@@ -18,9 +18,11 @@ function loadBooks() {
     let str = `<div class="wrap">`;
     for (let i = 0; i < books.length; i++) {
       str += `<div class="bookWrap">`;
+      str += `<p>${books[i].category}</p>`;
       str += `<div class="image"><img src="${books[i].imageCover}" /></div>`;
       str += `<h2>${books[i].name}</h2>`;
       str += `<p>${books[i].author}</p>`;
+      str += `<p>${books[i].price}</p>`;
       str += `</div>`;
     }
   
