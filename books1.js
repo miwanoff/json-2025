50f9c3c2e758622ae150832269a2bd08e54e4c41
miwanoff <marina.iva.noffa@gmail.com>
2025-04-13 15:13:39 +0300

    Add books1

---
+++ books1.js
@@ -0,0 +1,29 @@
+function loadBooks() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "books.json", true);
+    xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState != 4) return;
+      if (xhttp.status != 200) {
+        alert(xhttp.status + ": " + xhr.statusText);
+      } else {
+        let books = JSON.parse(xhttp.responseText);
+        insertBooks(books);
+      }
+    };
+  }
+  
+  function insertBooks(books) {
+    let str = `<div class="wrap">`;
+    for (let i = 0; i < books.length; i++) {
+      str += `<div class="bookWrap">`;
+      str += `<div class="image"><img src="${books[i].imageCover}" /></div>`;
+      str += `<h2>${books[i].name}</h2>`;
+      str += `<p>${books[i].author}</p>`;
+      str += `</div>`;
+    }
+  
+    str += `</div>`;
+    document.getElementById("books").innerHTML = str;
+  }
